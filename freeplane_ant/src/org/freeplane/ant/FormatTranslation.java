2d6a55b5243bb740da92497a32d33f0d7e791972
boercher <>
2010-03-23 18:33:08 +0100

    added fileheader

--- freeplane_ant/src/org/freeplane/ant/FormatTranslation.java
+++ freeplane_ant/src/org/freeplane/ant/FormatTranslation.java
@@ -1,3 +1,18 @@
+/**
+ * FormatTranslation.java
+ *
+ * Copyright (C) 2010,  Volker Boerchers
+ *
+ * FormatTranslation.java is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * FormatTranslation.java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 package org.freeplane.ant;
 
 import java.io.BufferedWriter;
--- freeplane_ant/src/org/freeplane/ant/FormatTranslationCheck.java
+++ freeplane_ant/src/org/freeplane/ant/FormatTranslationCheck.java
@@ -1,3 +1,18 @@
+/**
+ * FormatTranslationCheck.java
+ *
+ * Copyright (C) 2010,  Volker Boerchers
+ *
+ * FormatTranslationCheck.java is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * FormatTranslationCheck.java is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ */
 package org.freeplane.ant;
 
 import java.io.File;
