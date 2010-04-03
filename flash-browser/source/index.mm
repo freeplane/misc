61b5c66e1e642a2650a662864731641e360cb4da
dpolivaev <>
2010-04-03 17:43:44 +0200

    -

--- flash-browser/source/index.mm
+++ flash-browser/source/index.mm
@@ -2,9 +2,9 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="Flash browser" ID="Freemind_Link_661254052" CREATED="1107380732932" MODIFIED="1270302445515" COLOR="#000000">
 <font NAME="SansSerif" SIZE="20"/>
-<hook NAME="MapStyle" max_node_width="600"/>
+<hook NAME="MapStyle" background="#ffcccc" max_node_width="600"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node TEXT="Usage" POSITION="right" ID="ID_598187814" CREATED="1270302431018" MODIFIED="1270304433075" COLOR="#0033ff" HGAP="28" VSHIFT="-61">
+<node TEXT="Usage" POSITION="right" ID="ID_598187814" CREATED="1270302431018" MODIFIED="1270309364022" COLOR="#0033ff" HGAP="44" VSHIFT="-1">
 <font SIZE="18"/>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <node TEXT="Just a personal freemind flash browser." ID="ID_263966404" CREATED="1270304187103" MODIFIED="1270304187134" COLOR="#00b439">
@@ -15,13 +15,13 @@
 <font SIZE="16"/>
 <edge STYLE="bezier" WIDTH="thin"/>
 </node>
-<node TEXT="USE:" ID="ID_947118893" CREATED="1270304187134" MODIFIED="1270304433091" COLOR="#00b439">
+<node TEXT="USE:" ID="ID_947118893" CREATED="1270304187134" MODIFIED="1270309364038" COLOR="#00b439">
 <font SIZE="16"/>
 <edge STYLE="bezier" WIDTH="thin"/>
 <node TEXT="- insert in any browser page like in the example." ID="ID_1911705229" CREATED="1270304187149" MODIFIED="1270304350605" COLOR="#990000">
 <font SIZE="14"/>
 </node>
-<node TEXT="CONFIGURATION:" ID="ID_1976760068" CREATED="1270304187149" MODIFIED="1270304433106" COLOR="#990000">
+<node TEXT="CONFIGURATION:" ID="ID_1976760068" CREATED="1270304187149" MODIFIED="1270309364054" COLOR="#990000">
 <font SIZE="14"/>
 <node TEXT="All this variables can be added in the script. None of then if needed, they all have default values." ID="ID_949825492" CREATED="1270304187149" MODIFIED="1270304215892" COLOR="#111111">
 <font SIZE="12"/>
@@ -41,7 +41,7 @@
 </node>
 </node>
 </node>
-<node TEXT="Supported Icons" POSITION="right" ID="ID_514858580" CREATED="1270300856910" MODIFIED="1270304433122" COLOR="#0033ff" HGAP="93" VSHIFT="-76">
+<node TEXT="Supported Icons" POSITION="right" ID="ID_514858580" CREATED="1270300856910" MODIFIED="1270309364085" COLOR="#0033ff">
 <font SIZE="18"/>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <node TEXT="Numbers" ID="ID_408573909" CREATED="1270300866402" MODIFIED="1270304710285" COLOR="#00b439">
@@ -59,6 +59,8 @@
 <icon BUILTIN="full-0"/>
 </node>
 <node TEXT="Other icons" ID="ID_1758137548" CREATED="1270304786233" MODIFIED="1270304796873" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
 <icon BUILTIN="ksmiletris"/>
 <icon BUILTIN="smily_bad"/>
 <icon BUILTIN="gohome"/>
@@ -85,8 +87,6 @@
 <icon BUILTIN="wizard"/>
 <icon BUILTIN="button_ok"/>
 <icon BUILTIN="button_cancel"/>
-<font SIZE="16"/>
-<edge STYLE="bezier" WIDTH="thin"/>
 </node>
 </node>
 </node>
