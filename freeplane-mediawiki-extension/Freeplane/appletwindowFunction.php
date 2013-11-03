b04b5d8022b6dc1210fcd925d93db03634589d16
dpolivaev <dpolivaev>
2013-11-03 15:45:25 +0100

    update freeplane mediawiki extension to support jnlp and signed applets

--- freeplane-mediawiki-extension/Freeplane/appletwindowFunction.php
+++ freeplane-mediawiki-extension/Freeplane/appletwindowFunction.php
@@ -5,15 +5,15 @@ if($mm_title != ""){
 	$output = '
 <p style="text-align:center"><a href="'.$parameters['browsemode_initial_map'].'">'.$mm_title.'</a></p>';
 }
-$output .= '
-<APPLET CODE="org.freeplane.main.applet.FreeplaneApplet.class" ARCHIVE="'.$path.'freeplaneviewer.jar" WIDTH="100%" HEIGHT="'.$mm_height.'">';
+$output .= '<applet code="org.freeplane.main.applet.FreeplaneApplet.class" archive="'.$path.'freeplaneviewer.jar" jnlp_href="'.$path.'freeplane_applet.jnlp" width="100%" height="'.$mm_height.'">\n';
+$output .= '<script>document.write(\'<param name="location_href" value="\' + window.location.href +\'"/>\');</script>\n';
 	foreach ($parameters as $key => $value)
 		$output .="<PARAM NAME=\"$key\" VALUE=\"$value\">\n";
     if(! isset($parameters['selection_method']))
       	$output .="<PARAM NAME=\"selection_method\" VALUE=\"delayed\">\n";
         	$output .= '<PARAM NAME="type"  VALUE="application/x-java-applet;version=1.5">
-<PARAM NAME="scriptable"  VALUE="false">
-</APPLET>';
+<param name="scriptable" value="false">
+</applet>';
 return $output;
 }
 ?>
